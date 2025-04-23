b58bc63604e808e3e3ff7bc77e0598d0d9136517
aimebyiringiro <byiringiroaimeismael@gmail.com>
2025-04-23 07:41:19 -0500

    choosing a model

--- WORKSPACE/Assignment7/Assignment.r
+++ WORKSPACE/Assignment7/Assignment.r
@@ -14,6 +14,68 @@ dataset$Species = factor(dataset$Species,
 scaled_cols = scale(dataset[, 1:2])
 dataset[, 1:2] = scaled_cols
 
+library(e1071) # install.packages('e1071')
+#linear kernel
+classifier = svm(formula = Species ~ .,
+                 data = dataset,
+                 type = 'C-classification', # epds_regression is the default for SVR
+                 kernel = 'linear') # radial is the default for SVR
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = dataset)
+# Showing the Confusion Matrix and Accuracy
+library(caret)
+cm = confusionMatrix(y_pred, dataset$Species)
+print(cm$table)
+print(cm$overall['Accuracy'])
+
+
+#poly kernel
+classifier = svm(formula = Species ~ .,
+                 data = dataset,
+                 type = 'C-classification', # epds_regression is the default for SVR
+                 kernel = 'poly',
+                 degree = 3) # radial is the default for SVR
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = dataset)
+# Showing the Confusion Matrix and Accuracy
+library(caret)
+cm = confusionMatrix(y_pred, dataset$Species)
+print(cm$table)
+print(cm$overall['Accuracy'])
+
+
+#radial basis
+classifier = svm(formula = Species ~ .,
+                data = dataset,
+                type = 'C-classification', # epds_regression is the default for SVR
+                kernel = 'radial') # radial is the default for SVR
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = dataset)
+# Showing the Confusion Matrix and Accuracy
+library(caret)
+cm = confusionMatrix(y_pred, dataset$Species)
+print(cm$table)
+print(cm$overall['Accuracy'])
+
+
+
+
+#sigmoid 
+classifier = svm(formula = Species ~ .,
+                 data = dataset,
+                 type = 'C-classification', # epds_regression is the default for SVR
+                 kernel = 'sigmoid') # radial is the default for SVR
+# Predicting the Test set results
+y_pred = predict(classifier, newdata = dataset)
+# Showing the Confusion Matrix and Accuracy
+library(caret)
+cm = confusionMatrix(y_pred, dataset$Species)
+print(cm$table)
+print(cm$overall['Accuracy'])
+
+
+
+
 
 
 #
