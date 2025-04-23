c716b61ed9ea8de0d587cb4d79689945b0d9ef70
aimebyiringiro <byiringiroaimeismael@gmail.com>
2025-04-23 07:14:00 -0500

    add feature scaling

--- WORKSPACE/Assignment7/Assignment.r
+++ WORKSPACE/Assignment7/Assignment.r
@@ -8,3 +8,14 @@ dataset = read.csv('Iris_Data.csv')
 dataset$Species = factor(dataset$Species,
                         levels = c('Iris-setosa', 'Iris-versicolor', 'Iris-virginica'),
                         labels = c(0, 1, 2))
+
+
+# Feature Scaling
+scaled_cols = scale(dataset[, 1:2])
+dataset[, 1:2] = scaled_cols
+
+
+
+#
+
+
